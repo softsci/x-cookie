6c9a2acc978d4050e52216e6c14a3df2cb05257b
adon <xxd.nobug@gmail.com>
2025-03-06 12:36:30 +0800

    initial commit

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,9 +1,31 @@
 import { defineConfig } from "wxt";
+import obfuscatorPlugin from "vite-plugin-javascript-obfuscator";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   extensionApi: "chrome",
   modules: ["@wxt-dev/auto-icons", "@wxt-dev/module-react"],
+  vite: () => ({
+    plugins: [
+      obfuscatorPlugin({
+        options: {
+          compact: true,
+          controlFlowFlattening: true,
+          deadCodeInjection: true,
+          debugProtection: true,
+          disableConsoleOutput: true,
+          identifierNamesGenerator: "hexadecimal",
+          numbersToExpressions: true,
+          selfDefending: true,
+          simplify: true,
+          splitStrings: true,
+          stringArray: true,
+          stringArrayEncoding: ["base64"],
+          transformObjectKeys: true,
+        },
+      }),
+    ],
+  }),
   manifest: {
     permissions: ["tabs", "cookies", "scripting"],
     host_permissions: ["*://*.xiaohongshu.com/*", "*://*.x.com/*"],
